031e24fe4a058eb9b4509fe6bd8a66382d661849
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-26 15:52:51 +0200

    Refactor code
    Adding code-parts into functions where possible/useful

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const container = document.querySelector(".container");
 
+function createGrid(length=16) {
 
-//Create sub-containers, within container
-for(let i = 1; i <= 10; i++) {
-    const subContainer = document.createElement("div");
-    subContainer.className = "sub-container";
-    container.appendChild(subContainer);
+    //Create sub-containers, within container
+    for(let i = 1; i <= length; i++) {
+        const subContainer = document.createElement("div");
+        subContainer.className = "sub-container";
+        container.appendChild(subContainer);
+    }
+
+    const subContainer = document.querySelectorAll(".sub-container");
+    console.log(subContainer);
+
+    //Add container items to sub-containers
+    subContainer.forEach((subcontainer) => {
+        for(let i = 1; i <= length; i++) {
+            const newDivElement = document.createElement("div");
+            newDivElement.className = "container-item";
+            // newDivElement.setAttribute("style", "background-color: blue");
+            subcontainer.appendChild(newDivElement);
+        }
+    });
 }
 
-const subContainer = document.querySelectorAll(".sub-container");
-console.log(subContainer);
+createGrid();
 
-//Add container items to sub-containers
-subContainer.forEach((subcontainer) => {
-    for(let i = 1; i <= 10; i++) {
-        const newDivElement = document.createElement("div");
-        newDivElement.className = "container-item";
-        // newDivElement.setAttribute("style", "background-color: blue");
-        subcontainer.appendChild(newDivElement);
-    }
+//Add EventListener to div and change its background color on mouseover
+const allContainerItems = document.querySelectorAll(".container-item");
+
+allContainerItems.forEach((containerItem) => {
+    containerItem.addEventListener("mouseover", () => {
+        containerItem.setAttribute("style", "background-color: grey");
+    });
 });
+
+
 
